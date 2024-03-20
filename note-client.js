cfc7a0b4925156be14f29b1806fb302b084dea77
zeyadsleem <zeyadsleem1@gmail.com>
2024-03-20 10:49:52 +0200

    deploy to the web

--- note-client.js
+++ note-client.js
@@ -1,40 +1,40 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://notaty-dj6c.onrender.com/";
 
 async function addNote(noteData) {
-    const response = await fetch(`${baseUrl}/notes`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(noteData)
-    });
-    return response;
+  const response = await fetch(`${baseUrl}/notes`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(noteData),
+  });
+  return response;
 }
 
 async function updateNote(noteData) {
-    const response = await fetch(`${baseUrl}/notes`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(noteData)
-    });
-    return response;
+  const response = await fetch(`${baseUrl}/notes`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(noteData),
+  });
+  return response;
 }
 
 async function deleteNote(noteId) {
-    const response = await fetch(`${baseUrl}/notes/${noteId}`, {
-        method: "DELETE"
-    });
-    return response;
+  const response = await fetch(`${baseUrl}/notes/${noteId}`, {
+    method: "DELETE",
+  });
+  return response;
 }
 
 async function getNoteById(noteId) {
-    const response = await fetch(`${baseUrl}/notes/${noteId}`);
-    return response.json();
+  const response = await fetch(`${baseUrl}/notes/${noteId}`);
+  return response.json();
 }
 
 async function getNotes(noteTitle) {
-    let url = `${baseUrl}/notes`;
-    if (noteTitle) {
-        url += `/?title=${noteTitle}`;
-    }
-    const response = await fetch(url);
-    return response.json();
+  let url = `${baseUrl}/notes`;
+  if (noteTitle) {
+    url += `/?title=${noteTitle}`;
+  }
+  const response = await fetch(url);
+  return response.json();
 }
